c8b8beddf469c760dc355382bc6137e42c8516fb
Yiding He <yiding.he@gmail.com>
2015-07-27 12:12:20 +0800

    添加多服务器测试代码

--- src/test/java/multipleserver/TestMultipleServer.java
+++ src/test/java/multipleserver/TestMultipleServer.java
@@ -1,6 +1,11 @@
 package multipleserver;
 
+import com.hyd.appserver.ClientConfiguration;
+import com.hyd.appserver.MinaAppClient;
 import com.hyd.appserver.MinaAppServer;
+
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
 
 /**
  * todo: description
@@ -13,6 +18,19 @@ public class TestMultipleServer {
         startServer(8090);
         startServer(8091);
         startServer(8092);
+
+        startClient();
+    }
+
+    private static void startClient() {
+        ArrayList<InetSocketAddress> list = new ArrayList<InetSocketAddress>();
+        list.add(new InetSocketAddress("localhost", 8090));
+        list.add(new InetSocketAddress("localhost", 8091));
+        list.add(new InetSocketAddress("localhost", 8092));
+
+        ClientConfiguration conf = new ClientConfiguration();
+        conf.setServerAddresses(list);
+        MinaAppClient minaAppClient = new MinaAppClient(conf);
     }
 
     private static void startServer(int port) {
